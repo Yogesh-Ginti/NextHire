6ec5b87c9e956f750211abc626a14cad64393951
Yogesh-Ginti <yogeshginti@gmail.com>
2024-09-24 18:28:14 +0530

    update Function to save job in local storage in bookmarks

--- src/components/bookmark/Bookmark.jsx
+++ src/components/bookmark/Bookmark.jsx
@@ -5,32 +5,30 @@ import { useNavigate } from "react-router-dom";
 
 function Bookmark({ job }) {
   const { isUser } = useSelector((state) => state.auth);
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   // Function to save job in local storage
   const saveJob = (job) => {
-    if(!isUser){
-      navigate('/login')
-    }else{
-    const savedJobs = JSON.parse(localStorage.getItem("savedJobs")) || [];
-
-    // // Check if job is already saved
-    // const isJobSaved = savedJobs.some((savedJob) => savedJob.id === job.id);
-
-    // if (!isJobSaved) {
-    //   // Add job to the saved jobs array
-    //   const updatedSavedJobs = [...savedJobs, job];
-    //   localStorage.setItem("savedJobs", JSON.stringify(updatedSavedJobs));
-    //   alert(`Job ${job.title} saved!`);
-    // } else {
-    //   alert(`Job ${job.title} is already saved!`);
-    // }
-    // Add job to the saved jobs array
-    const updatedSavedJobs = [...savedJobs, job];
-    console.log('updated',updatedSavedJobs)
-    localStorage.setItem("savedJobs", JSON.stringify(updatedSavedJobs));
-    alert(`Job ${job.title} saved!`);
-  }
+    if (!isUser) {
+      navigate("/login");
+    } else {
+      const savedJobs = JSON.parse(localStorage.getItem("savedJobs")) || [];
+      if (savedJobs.length === 0) {
+        const updatedSavedJobs = [job];
+        localStorage.setItem("savedJobs", JSON.stringify(updatedSavedJobs));
+        alert(`Job ${job.title} saved!`);
+      } else {
+        const isJobSaved = savedJobs.some((savedJob) => savedJob.id === job.id);
+        if (!isJobSaved) {
+          // Add job to the saved jobs array
+          const updatedSavedJobs = [...savedJobs, job];
+          localStorage.setItem("savedJobs", JSON.stringify(updatedSavedJobs));
+          alert(`Job ${job.title} saved!`);
+        } else {
+          alert(`Job ${job.title} is already saved!`);
+        }
+      }
+    }
   };
   return (
     <span className="flex items-center justify-end">
